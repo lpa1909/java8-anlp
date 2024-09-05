cd1a2f77d618e63964a34e4c41eba5700106119f
lpa1909 <anlphe176714@fpt.edu.vn>
2024-09-05 16:55:30 +0700

    Spring Boot Project V1

---
+++ src/main/java/org/o7planning/springproject/SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package org.o7planning.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectApplication.class, args);
+    }
+
+}
